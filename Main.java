c7da8624e87fa86211fe85a8d95956b5c1ba8047
PorfilioVMJ <porfiliovmj@gmail.com>
2017-03-12 19:59:02 -0400

    Add Compatibility To HTML

--- Main.java
+++ Main.java
@@ -1,29 +1,47 @@
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetSocketAddress;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.Headers;
 
-public class Main{
+import javax.swing.text.html.HTMLDocument;
+
+public class Main {
 
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/code", new MyHandler());
+        HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
+        server.createContext("/", new IndexHandler());
+        server.createContext("/home", new IndexHandler());
         server.setExecutor(null);
         server.start();
+        System.out.println("VMJHttp Server Has Started!");
     }
 
-    static class MyHandler implements HttpHandler {
-        @Override
+    static class IndexHandler implements HttpHandler {
         public void handle(HttpExchange t) throws IOException {
-            String response = "Some random response";
-            t.sendResponseHeaders(200, response.length());
+           /*
+           C:/VMJHttp/code/index.html
+           */
+            String html;
+            StringBuilder contentBuilder = new StringBuilder();
+            try {
+                BufferedReader in = new BufferedReader(new FileReader("C:/VMJHttp/code/index.html"));
+                while((html = in.readLine()) != null){
+                    contentBuilder.append(html);
+                }
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String content = contentBuilder.toString();
+            t.sendResponseHeaders(200, content.length());
             OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
+            os.write(content.getBytes());
             os.close();
         }
     }
+
 
 }
